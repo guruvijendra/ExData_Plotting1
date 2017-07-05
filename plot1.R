af1dab9f73cc86874a3fab394efb52137860164f
guruvijendra <guru.2110@gmail.com>
2017-07-05 15:09:56 -0700

    Add files via upload

--- plot1.R
+++ plot1.R
@@ -1,4 +1,4 @@
 setwd("/Users/guruprasadvijendra/datasciencecoursera/EDA-Week1")
 df <- read.table("household_power_consumption 2.txt",header = TRUE, sep = ";", na.strings = "?")
-options(scipen = 5)
-hist(df$Global_active_power,xlab = "Global Active Power(kilowatts)",ylab = "Frequency",main = "Global Active Power",col="red")
+subsetdf<-subset(df, Date %in% c("1/2/2007","2/2/2007"))
+hist(subsetdf$Global_active_power,xlab = "Global Active Power(kilowatts)",ylab = "Frequency",main = "Global Active Power",col="red")
